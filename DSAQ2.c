bcba0c37dd8a5db30f19f73822b7b21276a2eaca
Daksh Verma <139080021+Agsbg@users.noreply.github.com>
2026-02-06 00:48:56 +0530

    Initial Commit
    Implement a program to delete an element from an array.

---
+++ DSAQ2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (scanf("%d", &pos) != 1) return 0;
+
+    for (int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d%s", arr[i], (i == n - 2) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
