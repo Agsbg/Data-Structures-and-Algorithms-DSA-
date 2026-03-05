8d1812eba390c9f7cc25c007af330ea37f2d88a4
Daksh Verma <139080021+Agsbg@users.noreply.github.com>
2026-03-05 15:01:35 +0530

    DSAQ4.c

---
+++ DSAQ4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int left = 0;
+    int right = n - 1;
+    
+    while (left < right) {
+        // Swap elements at left and right pointers
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    reverseArray(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
