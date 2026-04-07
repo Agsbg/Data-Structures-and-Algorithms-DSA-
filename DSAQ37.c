e5b9081dc9e4ef0350aefe0265a4ba73ebb5b560
Daksh Verma <139080021+Agsbg@users.noreply.github.com>
2026-04-07 21:19:02 +0530

    Initial Commit

---
+++ DSAQ37.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int pq[MAX];
+int size = 0;
+
+// Insert element (keep array sorted)
+void insert(int x) {
+    int i = size - 1;
+
+    // Shift elements to make correct position
+    while (i >= 0 && pq[i] > x) {
+        pq[i + 1] = pq[i];
+        i--;
+    }
+
+    pq[i + 1] = x;
+    size++;
+}
+
+// Delete (remove highest priority = smallest element)
+int deletePQ() {
+    if (size == 0) return -1;
+
+    int val = pq[0];
+
+    // Shift all elements left
+    for (int i = 1; i < size; i++) {
+        pq[i - 1] = pq[i];
+    }
+
+    size--;
+    return val;
+}
+
+// Peek (get smallest element)
+int peek() {
+    if (size == 0) return -1;
+    return pq[0];
+}
+
+int main() {
+    int n, x;
+    char op[10];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if (strcmp(op, "delete") == 0) {
+            printf("%d\n", deletePQ());
+        }
+        else if (strcmp(op, "peek") == 0) {
+            printf("%d\n", peek());
+        }
+    }
+
+    return 0;
+}
